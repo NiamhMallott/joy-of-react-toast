226fe3bc8e4eea9b7f7cfd2d6b3c265c0d517bbb
Niamh Mallott <niamhmallott@hotmail.co.uk>
2023-09-27 09:55:00 +0100

    adding aria for screen readers to the toast

--- src/components/ToastShelf/ToastShelf.js
+++ src/components/ToastShelf/ToastShelf.js
@@ -3,14 +3,21 @@ import React from "react";
 import Toast from "../Toast";
 import styles from "./ToastShelf.module.css";
 import { ToastContext } from "../ToastProvider/ToastProvider";
+import VisuallyHidden from "../VisuallyHidden";
 
 function ToastShelf() {
   const { toasts } = React.useContext(ToastContext);
 
   return (
-    <ol className={styles.wrapper}>
+    <ol
+      className={styles.wrapper}
+      role="region"
+      aria-live="polite"
+      aria-label="notification"
+    >
       {toasts.map(({ id, type, message }) => (
         <li className={styles.toastWrapper} key={id}>
+          <div className={VisuallyHidden.wrapper}>{type}</div>
           <Toast id={id} type={type}>
             {message}
           </Toast>
