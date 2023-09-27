2dbe112f70897e1ff4a74a9b97ceb4eeaa997087
Niamh Mallott <niamhmallott@hotmail.co.uk>
2023-09-27 09:23:47 +0100

    create a useEffect to create an escape for toasts for keyboard users.

--- src/components/ToastProvider/ToastProvider.js
+++ src/components/ToastProvider/ToastProvider.js
@@ -5,6 +5,20 @@ export const ToastContext = React.createContext();
 export function ToastProvider({ children }) {
   const [toasts, setToasts] = React.useState([]);
 
+  React.useEffect(() => {
+    function handleKeydown(event) {
+      if (event.code === "Escape") {
+        setToasts([]);
+      }
+    }
+
+    window.addEventListener("keydown", handleKeydown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeydown);
+    };
+  }, []);
+
   const removeToast = React.useCallback((id) => {
     setToasts((toasts) => toasts.filter((toast) => toast.id !== id));
   }, []);
